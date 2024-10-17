084e7d9173f2b9038d965a15b1157855992ce902
Brenno Kevyn <brennokm@gmail.com>
2024-10-17 09:49:10 -0300

    Update WS.bat
    - Bibliotecas necessárias para o uso do programa

--- WS.bat
+++ WS.bat
@@ -1,1 +1,9 @@
-pyinstaller --onefile --noconsole --name=Warspear-AutoCraft --icon=imgs/icones/icon_ws_mkt.ico pys/main.py
+python.exe -m pip install --upgrade pip
+pip install pyautogui
+pip install pynput
+pip install requests
+pip install python-dotenv
+pip install pillow
+pip install pyinstaller
+
+pyinstaller --onefile --name=Warspear-AutoCraft --icon=imgs/icones/icon_ws_mkt.ico pys/main.py
